5f0542bf5c62a1c097352ac772d850ef3d6de1dd
Replit user <>
2023-05-12 13:11:37 +0000

    new try

--- heap.c
+++ heap.c
@@ -35,12 +35,11 @@ void heap_push(Heap* pq, void* data, int priority){
   pq->size++;
 
   
-  if(pq ->heapArray[pq->size].priority > pq ->heapArray[pq->size/2].priority){
-    printf("help\n");
+  if(pq ->heapArray[pq->size].priority > pq ->heapArray[(pq->size-1)/2].priority){
     for(int i= pq->capac; i> 0; i= i/2){
       heapElem aux =pq ->heapArray[i];
-      pq ->heapArray[i] = pq ->heapArray[i/2];
-      pq ->heapArray[i/2] = aux;
+      pq ->heapArray[i] = pq ->heapArray[(i-1)/2];
+      pq ->heapArray[(i-1)/2] = aux;
       
     }
     
